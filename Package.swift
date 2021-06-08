97b4d21ef49156bf74bbd07d5f1a2b5f42f98d09
Aliaksandr Vecharou <vecerow@gmail.com>
2021-06-08 19:46:31 +0300

    added spm support

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "TTTAttributedLabel",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "TTTAttributedLabel",
+            targets: ["TTTAttributedLabel"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "TTTAttributedLabel",
+            dependencies: [])
+    ]
+)
